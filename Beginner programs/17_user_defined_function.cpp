fac1a36844a72e067c9e2d8cf4fba5d6e6a72165
JkayJatan <jatankumarjk806@gmail.com>
2025-08-03 12:22:25 +0530

    Introduction to user defined functions

---
+++ Beginner programs/17_user_defined_function.cpp
@@ -0,0 +1,13 @@
+// Creating a user defined function
+#include<iostream>
+using namespace std;
+void greet();
+int main(){
+    greet();
+    greet();
+    greet();
+    return 0;
+}
+void greet(){
+    cout<<"Hello Friends!\n";
+}
