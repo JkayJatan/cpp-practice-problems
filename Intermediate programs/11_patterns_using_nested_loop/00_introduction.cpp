780d7629da736b5fb47d97071c224635abca072b
JkayJatan <jatankumarjk806@gmail.com>
2025-08-07 23:17:47 +0530

    Paterns using nested loops

---
+++ Intermediate programs/11_patterns_using_nested_loop/00_introduction.cpp
@@ -0,0 +1,22 @@
+/*
+#print the below pattern using nested loop
+*****
+*****
+*****
+*****
+*****
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
