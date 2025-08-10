dcb5642748b9d2384b8d412d37b299d8e943c2f5
JkayJatan <jatankumarjk806@gmail.com>
2025-08-10 21:36:31 +0530

    Add pattern 9

--- Intermediate programs/11_patterns_using_nested_loop/09_pattern.cpp
+++ Intermediate programs/11_patterns_using_nested_loop/09_pattern.cpp
@@ -11,7 +11,7 @@ int main(){
     cout<<"Enter n:";
     cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=0;j<=n-i;j++){
+        for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
         for (int j=0;j<=i;j++){
